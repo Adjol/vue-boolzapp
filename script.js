99a4c21aa258e55fe571e7952e863350b22cee5c
Adjol <ad.mitrushi@gmail.com>
2023-12-14 12:16:26 +0100

    back-ground & layout

---
+++ script.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return {
+            arr: ['c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o','c', 'i', 'a', 'o']
+        }
+    },
+
+    methods:{
+
+    },
+
+
+    mounted() {
+
+
+    }
+}).mount("#app");
